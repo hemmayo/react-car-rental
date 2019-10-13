83c5d59501a4587f3ccb6721ff7ef8c9bad53bab
Emmanuel Popoola <olaayo10@gmail.com>
2019-10-13 06:15:57 +0100

    car sensor update

--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,12 @@ admin.initializeApp();
 // });
 
 exports.driver_sensor_update = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
   const { uid, lat, lng, angle } = req.query;
   const ref = admin.database().ref(`drivers/${uid}`);
   const sensorData = {
     lat: Number(lat),
     lng: Number(lng),
-    angle: Number(lng),
+    angle: Number(angle),
     lastUpdated: new Date().toString()
   };
   await ref
@@ -36,4 +35,29 @@ exports.driver_sensor_update = functions.https.onRequest(async (req, res) => {
     });
 
   //   res.redirect(303, snapshot.ref.toString());
+});
+
+exports.car_sensor_update = functions.https.onRequest(async (req, res) => {
+  const { uid, lat, lng, angle } = req.query;
+  const ref = admin.database().ref(`cars/${uid}`);
+  const sensorData = {
+    lat: Number(lat),
+    lng: Number(lng),
+    angle: Number(angle),
+    lastUpdated: new Date().toString()
+  };
+  await ref
+    .once("value")
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        ref.child("sensorData").set({ ...sensorData });
+        res.send("success");
+      } else {
+        res.send("driver doesn't exist");
+      }
+      return sensorData;
+    })
+    .catch(error => {
+      res.redirect(500, JSON.stringify(error));
+    });
 });
